3990cc1bb8bc2950859cb76926064db8cefd06d5
Jusiiek <jakub.zurawski@outlook.com>
2024-01-15 20:30:48 +0100

    added useradmin to admin registered urls

--- api/user/admin.py
+++ api/user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Role, UserRole
+from .forms import UserForm
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'get_role', 'is_active', 'is_staff', 'is_superuser')
+    exclude = ('is_staff', 'is_superuser')
+    form = UserForm
+
+    def get_form(self, request, obj=None, **kwargs):
+        kwargs['form'] = UserForm
+        form = super().get_form(request, obj=None, **kwargs)
+        available_roles = ["SUPER_ADMIN", "ADMIN", "USER"]
+        form.base_fields['role'].choices = available_roles
+        if obj and obj.role:
+            form.base_fields['role'].initial = [obj.role.name]
+        return form
+
+    def get_role(self, user):
+        return user.role
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Role)
+admin.site.register(UserRole)
